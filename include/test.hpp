01bf2351c1f4575e57190929e6263e9f96cf4fe4
Андрей <okdahorosho@gmail.com>
2017-05-19 01:52:25 +0300

    Update test.hpp

--- include/test.hpp
+++ include/test.hpp
@@ -11,14 +11,10 @@ public:
 
 	Complex_t() : real(0), imaginary(0) {};
 	Complex_t(T real, T imaginary) :real(real), imaginary(imaginary) {};
-	
-  
   Complex_t operator + (const Complex_t &complex)
   {
 	return Complex_t(real + complex.real, imaginary + complex.imaginary);
 	}
-
-　
 	Complex_t operator - (const Complex_t &complex)
 	{
 		return Complex_t(real - complex.real, imaginary - complex.imaginary);
@@ -41,21 +37,16 @@ public:
 		imaginary *= complex.imaginary;
 		return (*this);
 	}
-
 	Complex_t operator * (const Complex_t &complex)
 	{
 		return Complex_t(real * complex.real - imaginary * complex.imaginary, real * complex.imaginary + imaginary * complex.real);
 	}
-
-　
 	Complex_t operator / (const Complex_t &complex)
 	{
 		Complex_t temp{ 0.0,0.0 };
-
 		double k = complex.real * complex.real + complex.imaginary * complex.imaginary;
 		if (k == 0)
 			exit(1);
-
 		temp.real = (real * complex.real + imaginary * complex.imaginary) / k;
 		temp.imaginary = (imaginary * complex.real - real * complex.imaginary) / k;
 
@@ -74,3 +65,4 @@ public:
 			return 0;
 		}
 		return 1;
+	}
