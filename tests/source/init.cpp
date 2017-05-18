d7d193fec1c169b227ab70b471526ad32cce05f6
Андрей <okdahorosho@gmail.com>
2017-05-19 01:03:19 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -1,12 +1,12 @@
-#include <testclass.hpp>#include <catch.hpp>
-Complex<double> a {5.0, 7.0};
-Complex<double> b {5.0, 7.0};
+#include <test.hpp>#include <catch.hpp>
+Complex<double> a {7.0, 9.0};
+Complex<double> b {7.0, 9.0};
 SCENARIO ("constructorTest")
 {	
  int Real = x.getReal();	
  int Imag = x.getIm();	
- REQUIRE (Real == 10);	
- REQUIRE (Imag == 15);
+ REQUIRE (Real == 5);	
+ REQUIRE (Imag == 10);
 }
 
 SCENARIO ("Addition"){	
@@ -26,15 +26,15 @@ SCENARIO ("MultiNum")
 {	comp.mult(x,2);	
  int Real = comp.getReal();	
  int Imag = comp.getIm();	
- REQUIRE (Real == 20);	
- REQUIRE (Imag == 30);
+ REQUIRE (Real == 10);	
+ REQUIRE (Imag == 20);
 }
 SCENARIO ("DivNum")
 {	comp.div(x,5);	
  int Real = comp.getReal();	
  int Imag = comp.getIm();	
- REQUIRE (Real == 2);	
- REQUIRE (Imag == 3);
+ REQUIRE (Real == 1);	
+ REQUIRE (Imag == 2);
 }
 SCENARIO ("DivNumZero","[!mayfail]")
 {	comp.div(x,0);	
@@ -47,6 +47,6 @@ SCENARIO("MultiComplex")
 {	comp.multC(x,y);	
  int Real = comp.getReal();	
  int Imag = comp.getIm();	
- REQUIRE (Real == -55);	
- REQUIRE (Imag == 80);
+ REQUIRE (Real == -75);	
+ REQUIRE (Imag == 100);
 }
