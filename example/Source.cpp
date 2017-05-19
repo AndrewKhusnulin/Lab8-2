9420f66923089ddbc53933ea50e258680bfbcb19
Андрей <okdahorosho@gmail.com>
2017-05-19 09:53:51 +0300

    Update Source.cpp

--- example/Source.cpp
+++ example/Source.cpp
@@ -1,25 +1,5 @@
-#include"test.hpp"
-int main()
-{
-	Complex_t<double> a{ 7.0, 9.0 };
-	
-	Complex_t<double> b{ 7.0,9.0 };
-	
-	a == b; 
-	
-	a += a;
-	//cout << "a+=a" << a << endl;
-	b -= {3, 5};
-	//cout << "b-={3,5} " << b << endl;
-	a *= b;
-	a+b;
-	a*b;
-	a-b;
-	a/b;
-	/*cout << "a+b=" << a + b << endl;
-	cout << "a*b=" << a*b << endl;
-	cout << "a-b=" << a - b << endl;
-	cout << "a/b=" << a / b << endl;*/
+#include "test.hpp"
+int main() {
 
 	system("pause");
 	return 0;
